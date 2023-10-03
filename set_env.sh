b113c79d2d7ddf6d16712e6fa44bf67dbada1fdc
LocN1 <nguyenloc27021999@gmail.com>
2023-10-03 21:29:10 +0700

    done part 2

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgrestest
+export POSTGRES_PASSWORD=mypassword
+export POSTGRES_HOST=postgrestest.cwb1fio8s3ex.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgrestest
+export AWS_BUCKET=udagram-locn1
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
